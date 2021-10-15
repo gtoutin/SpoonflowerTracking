8b8f21365440e5a17484e583bd94a31a5b849a2a
NerdBird <nerdbird@pop-os.localdomain>
2021-10-14 23:45:52 -0500

    Adds second command line option to support zenity in linux

--- shopcheck.py
+++ shopcheck.py
@@ -4,23 +4,32 @@ import os
 import sys
 
 
-
+OS = "mac"
 FAIL_MESSAGE = "Uh oh! Something's up with your Spoonflower shop! Go check it out."
 APP_TITLE = "Daily Spoonflower Shop Check"
 
 def main():
     page_data = str(requests.get(f"https://www.spoonflower.com/profiles/{SHOP_NAME}").content)
     if (re.search(rf"by {SHOP_NAME}", page_data) is None):
-        notify(APP_TITLE, FAIL_MESSAGE) # SOMETHING IS WRONG
+        notify(APP_TITLE, FAIL_MESSAGE, OS) # SOMETHING IS WRONG
 
 
-def notify(title, message):
+def notify(title, message, opsys):
     """Notify the macOS user their shop is messed up"""
-    os.system(f"""
-    osascript -e 'display notification "{message}" with title "{title}"'
-    """)
+    if (opsys == "mac"):
+        os.system(f"""
+        osascript -e 'display notification "{message}" with title "{title}"'
+        """)
+    elif (opsys == "linux"):
+        os.system(f"""zenity --info --text="{message}" """)
 
 
 if __name__ == '__main__':
-    SHOP_NAME = sys.argv[1]
+    try:
+        SHOP_NAME = sys.argv[1]
+    except:
+        print("Expecting a command line argument SHOP_NAME")
+        exit
+    if (len(sys.argv) == 3):
+        OS = sys.argv[2]
     main()
